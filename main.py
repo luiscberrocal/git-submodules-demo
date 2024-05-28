6ffa7ac7b85cd7a5fd4209cfb6c2360823aedc05
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-05-28 14:59:06 -0500

    Configuration

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 # This is a sample Python script.
-from simple_mail.email.schema import SenderConfig
+import os
+
+from config import load_environment_variables
+from simple_mail.email.mailer import send_email
+from simple_mail.email.schema import EmailMessage, SenderConfig
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 if __name__ == "__main__":
-    sender_config = SenderConfig(email="test", password="test")
-    print(sender_config)
+    load_environment_variables(environment_filename="email_vars.txt", source_folder_name=".secrets")
+    username = os.getenv("EMAIL")
+    pwd = os.getenv("PASSWORD")
+    print(f"Username: {username}")
+    print(f"Password: {pwd}")
 
+    sender_config = SenderConfig(email=username, password=pwd)
+    email_message = EmailMessage(
+        recipients=["luis.berrocal.1942@gmail.com"],
+        subject="Submodule test",
+        content="This is a test email from the submodule.",
+        sender_config=sender_config
+    )
+    send_email(email_message=email_message)
+    print("Email sent!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
